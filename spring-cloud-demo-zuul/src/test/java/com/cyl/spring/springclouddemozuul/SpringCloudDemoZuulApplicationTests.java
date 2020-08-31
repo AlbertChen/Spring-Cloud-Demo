5c8bc9390f5a5ee89fbe2cf6b979300d2d2fea10
chenyiliang <yiliang.chen@gocmx.com>
2020-08-31 18:13:39 +0800

    zuulj

---
+++ spring-cloud-demo-zuul/src/test/java/com/cyl/spring/springclouddemozuul/SpringCloudDemoZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cyl.spring.springclouddemozuul;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudDemoZuulApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
